8e4ca44c42590ca698120e453e100aee58eadf8a
SuryanshBisht <suryansh4bisht@gmail.com>
2022-07-13 23:04:08 +0530

    day 12 third

---
+++ Day9/49_Subset_sums.cpp
@@ -0,0 +1,26 @@
+// https://practice.geeksforgeeks.org/problems/subset-sums2234/1
+class Solution {
+    vector<vector<int>> ans;
+    vector<int> v;
+public:
+    void subsets(vector<int> &nums, int ind){
+        if(ind == nums.size()){
+            ans.push_back(v);
+            return;
+        }
+           
+        int i = ind;
+        while(ind < nums.size() && nums[i] == nums[ind]) ind++;
+        subsets(nums, ind);
+        v.push_back(nums[i]);
+        subsets(nums, i + 1);
+        v.pop_back();
+    }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        ans.clear();
+        sort(nums.begin(), nums.end());
+        subsets(nums, 0);
+        return ans;
+    }
+};
